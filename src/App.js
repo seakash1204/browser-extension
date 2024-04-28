4d76fbc4594a139f9826af91b72d438e43857675
Akash Kumar <117556687+seakash1204@users.noreply.github.com>
2024-04-28 07:15:45 +0530

    Initial Setup Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import './App.css'
+import {images} from './db/images'
+
 function App() {
+
+  const index = Math.floor(Math.random() * images.length)
+  const bgImage = images[index].image;
+
   return (
-    <div className="App">
+    <div className="app" style={{backgroundImage : `url("${bgImage}")`}}>
       <h2>Browser Extension</h2>
     </div>
   );
